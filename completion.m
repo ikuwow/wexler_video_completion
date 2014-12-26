a4f3b7bd71da2f3aba8ae5bd79c47e75590aa520
Ikuo Degawa <degawa@ikuwow.com>
2014-12-26 14:16:54 +0900

    input images and directory output

--- completion.m
+++ completion.m
@@ -1,6 +1,38 @@
 % completion.m
 
+% implementation plan
+%{
+* input and output
+* search appropriate patch and apply, loop.
+* alpha
+* update with ML
+* process multiple scale and propagate
+* patchmatch
+%}
+
 
 function output = completion(input,mask)
+
+%% pseudo code
+%{
+for scales, corsest to finer (input and database)
+    while not converge 
+        for all p in H
+            let all windows W containing p
+            find best matching V of patch with p
+            let ci be the appropriate colors
+            set weight w
+            S^(t+1) (p) <= ML(c,w) using eq (4) （mean shift algorithm）
+        end 
+        t = t+1; (t is parameter of EM algorithm)
+    endwhile
+
+    propagate to the next level (used in ML step)
+
+end 
+
+%}
+
 output = input;
+
 end
--- test.m
+++ test.m
@@ -1,8 +1,22 @@
 clear
 close all
 
-testImg = ones([512,512,3]);
-mask = zeros(size(testImg));
+testImageName = 'lena'
+
+testImagePath = '~/Documents/MATLAB/AutoShared/testimages/512/';
+testImageSource = fullfile(testImagePath,testImageName);
+
+testImg = imread(fullfile([testImageSource,'.bmp'])); % uint8
+
+% mask creation
+mask = ones([size(testImg,1),size(testImg,2)],'uint8');
+mask(240:260,240:260) = 0;
+
+maskedImg = repmat(mask,[1,1,3]).*testImg;
 resultImg = completion(testImg,mask);
 
 disp('done');
+
+figure(1),imshow(uint8(testImg)),title('Original Image')
+figure(2),imshow(uint8(maskedImg)),title('Masked Image')
+figure(3),imshow(uint8(resultImg)),title('Inpainted Image')
