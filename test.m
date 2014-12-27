56ab13c5fe0379c1d81cac2f1885cf550eadecb6
Ikuo Degawa <degawa@ikuwow.com>
2014-12-27 14:52:13 +0900

    Import predefined masks

--- test.m
+++ test.m
@@ -9,13 +9,23 @@ testImageSource = fullfile(testImagePath,testImageName);
 testImg = imread(fullfile([testImageSource,'.bmp'])); % uint8
 
 % mask creation
+%{
 mask = ones([size(testImg,1),size(testImg,2)],'uint8');
 mask(240:260,240:260) = 0;
+%}
 
-maskedImg = repmat(mask,[1,1,3]).*testImg;
+% mask import
+masks = load('~/Documents/MATLAB/AutoShared/testimages/mask512.mat');
+mask = logical(masks.text);
+% mask = masks.line;
+%}
+
+maskedImg = repmat(uint8(mask),[1,1,3]).*testImg;
+
+%% Main 
+disp('Completion Start.');
 resultImg = completion(testImg,mask);
-
-disp('done');
+disp('Completion Done!');
 
 figure(1),imshow(uint8(testImg)),title('Original Image')
 figure(2),imshow(uint8(maskedImg)),title('Masked Image')
